b4d249111aa648daa404d217c03e077e365eb7d0
Niamot <58494481+niamotullah@users.noreply.github.com>
2025-07-15 13:09:38 +0600

    Initialize a empty repo

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
